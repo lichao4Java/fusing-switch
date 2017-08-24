82ab7897a4e452f6d068f55721c430505a239cb1
lichao <lichaoever@hotmail.com>
2017-08-24 17:46:25 +0800

    first commit

--- src/test/java/com/qding/fusing/A.java
+++
@@ -1,7 +0,0 @@
-package com.qding.fusing;
-
-public interface A {
-
-	public String helloworld(String name);
-	
-}
--- src/test/java/com/qding/fusing/AImpl.java
+++
@@ -1,9 +0,0 @@
-package com.qding.fusing;
-
-public class AImpl implements A{
-
-	public String helloworld(String name) {
-		throw new RuntimeException("AAAAAAAAAAAA");
-	}
-
-}
